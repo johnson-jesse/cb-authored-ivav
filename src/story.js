616595c09f37dfb9d0a2091be392440043e05976
Jesse A Johnson <jesseajohnson@pm.me>
2020-09-23 16:24:54 -0400

    slightly more concrete example

--- src/story.js
+++ src/story.js
@@ -1,6 +1,20 @@
 runner = (driver) => {
-    //console.log('I am run:', driver);
-    return `I am run: ${driver}`;
+    const log = [`[TESTING] login-form: ${Date.now}`];
+
+    try {
+        const url = 'http://crossbrowsertesting.github.io/login-form.html';
+        log.push[`[TRYING] ${url}`];
+        await driver.get(url);
+
+        log.push['[TRYING] snapshot'];
+        await driver.takeSnapshot();
+    } catch(e) {
+        log.push[`[ERROR] ${e}`]
+    }
+
+    log[`[DONE] login-form:  ${Date.now}`];
+
+    return log;
 };
 
 numberOfStories = 1;
