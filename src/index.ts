4708d51113ec377d7e2ddac7b5a68b536a0b7623
Jesse A Johnson <jesseajohnson@pm.me>
2020-09-24 11:09:36 -0400

    unlocking get

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,22 @@ var runner = async (driver: any): Promise<string[]> => {
 
     try {
         log.push(`[LOADING] ${Date.now()}`);
-        //await driver.get(url);
+        await driver.get(url);
 
-        log.push('[TRYING] send username to field');
+        //log.push('[TRYING] send username to field');
         //await driver.findElement(webdriver.By.id("username")).sendKeys("tester@crossbrowsertesting.com");
 
-        log.push('[TRYING] send password to field');
+        //log.push('[TRYING] send password to field');
         //send keys to element to enter text
         //await driver.findElement(webdriver.By.xpath("//*[@type=\"password\"]")).sendKeys("test123");
 
-        log.push('[TRYING] submit form');
+        //log.push('[TRYING] submit form');
         //await driver.findElement(webdriver.By.css("button[type=submit]")).click();
 
-        log.push('[TRYING] verify results');
+        //log.push('[TRYING] verify results');
         //await driver.wait(webdriver.until.elementLocated(webdriver.By.id("logged-in-message")), 10000);
 
-        log.push('[TRYING] close down');
+        //log.push('[TRYING] close down');
         //await driver.quit();
     } catch (e) {
         log.push(`[ERROR] ${e}`);
