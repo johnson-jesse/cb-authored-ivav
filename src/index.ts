719d59a2efd1c2f4b7c6eac19929e8acafdfe465
Jesse A Johnson <jesseajohnson@pm.me>
2020-09-24 09:03:38 -0400

    removing console log

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-import { WebDriver } from 'selenium-webdriver';
+// import { webdriver } from 'selenium-webdriver';
 
-const runner = (driver: WebDriver) => {
-    console.log('I am run:', driver);
-}
+const runner = async (driver: any): Promise<string[]> => {
+    const url = 'http://crossbrowsertesting.github.io/login-form.html';
+
+    const log = [`[TESTING] login-form: ${Date.now()}`];
+    log.push(`[SUBJECT] ${url}`);
+
+    try {
+        log.push(`[LOADING] ${Date.now()}`);
+        //await driver.get(url);
+
+        log.push('[TRYING] send username to field');
+        //await driver.findElement(webdriver.By.id("username")).sendKeys("tester@crossbrowsertesting.com");
+
+        log.push('[TRYING] send password to field');
+        //send keys to element to enter text
+        //await driver.findElement(webdriver.By.xpath("//*[@type=\"password\"]")).sendKeys("test123");
+
+        log.push('[TRYING] submit form');
+        //await driver.findElement(webdriver.By.css("button[type=submit]")).click();
+
+        log.push('[TRYING] verify results');
+        //await driver.wait(webdriver.until.elementLocated(webdriver.By.id("logged-in-message")), 10000);
+
+        log.push('[TRYING] close down');
+        //await driver.quit();
+    } catch (e) {
+        log.push(`[ERROR] ${e}`);
+        //await driver.quit();
+    }
+
+    log.push(`[DONE] login-form:  ${Date.now()}`);
+
+    return log;
+};
+
+const numberOfStories = 1;
--- src/story.js
+++ src/story.js
@@ -1,3 +1,5 @@
+// import { webdriver } from 'selenium-webdriver';
+
 runner = async (driver) => {
     const url = 'http://crossbrowsertesting.github.io/login-form.html';
 
@@ -6,25 +8,26 @@ runner = async (driver) => {
 
     try {
         log.push(`[LOADING] ${Date.now()}`);
-        await driver.get(url);
+        //await driver.get(url);
 
         log.push('[TRYING] send username to field');
-        await driver.findElement(webdriver.By.id("username")).sendKeys("tester@crossbrowsertesting.com");
+        //await driver.findElement(webdriver.By.id("username")).sendKeys("tester@crossbrowsertesting.com");
 
         log.push('[TRYING] send password to field');
         //send keys to element to enter text
-        await driver.findElement(webdriver.By.xpath("//*[@type=\"password\"]")).sendKeys("test123");
+        //await driver.findElement(webdriver.By.xpath("//*[@type=\"password\"]")).sendKeys("test123");
 
         log.push('[TRYING] submit form');
-        await driver.findElement(webdriver.By.css("button[type=submit]")).click();
+        //await driver.findElement(webdriver.By.css("button[type=submit]")).click();
 
         log.push('[TRYING] verify results');
-        await driver.wait(webdriver.until.elementLocated(webdriver.By.id("logged-in-message")), 10000);
+        //await driver.wait(webdriver.until.elementLocated(webdriver.By.id("logged-in-message")), 10000);
 
         log.push('[TRYING] close down');
-        await driver.quit();
+        //await driver.quit();
     } catch (e) {
         log.push(`[ERROR] ${e}`);
+        //await driver.quit();
     }
 
     log.push(`[DONE] login-form:  ${Date.now()}`);
