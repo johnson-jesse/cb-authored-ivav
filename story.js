1994c371481b87400d18eb66710fe05d5fe49749
Jesse A Johnson <jesseajohnson@pm.me>
2020-09-27 16:17:46 -0400

    updated file ames

---
+++ story.js
@@ -0,0 +1,35 @@
+"use strict";
+const webdriver = require('selenium-webdriver');
+const { nanoid } = require('nanoid');
+const options = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+    hour12: false, timeZone: 'UTC', timeZoneName: 'short'
+};
+const utcNow = () => new Intl.DateTimeFormat('en-US', options).format(Date.now());
+const timeInSeconds = (start) => (Date.now() - start) / 1000;
+const mklog = (story, message) => `[${story}] ${utcNow()} ${message}`;
+const runner = {
+    uuid: nanoid(),
+    numberOfStories: 1,
+    start: async (driver) => {
+        const results = [];
+        const name = 'login-form';
+        const start = Date.now();
+        const url = 'http://crossbrowsertesting.github.io/login-form.html';
+        const log = (message) => {
+            results.push(mklog(name, message));
+        };
+        log('started');
+        log(`subject: ${url}`);
+        try {
+            log(`loading`);
+        }
+        catch (e) {
+            log(`error: ${e}`);
+        }
+        log(`finished in ${timeInSeconds(start)} seconds}`);
+        return results;
+    }
+};
+module.exports = runner;
